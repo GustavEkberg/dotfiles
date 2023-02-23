bfa01ce8040926a1070f0f8800dc21b998800d20
GustavEkberg <krypteratkadaver@gmail.com>
2023-02-23 15:09:39 +0100

    nvim: update visual mode colors

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -5,6 +5,7 @@ if not status_ok then
 	return
 end
 
+vim.cmd("hi Visual  guifg=#000000 guibg=#e8e7ff gui=none")
 -- vim.cmd("highlight NonText guibg=none")
 -- vim.cmd("highlight Normal guibg=none")
 -- require(colorscheme).setup({
