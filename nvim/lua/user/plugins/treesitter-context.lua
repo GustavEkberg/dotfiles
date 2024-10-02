e361c0d66a7bd424ce1e59c9c31fb9c042cb5848
GustavEkberg <krypteratkadaver@gmail.com>
2024-10-02 10:23:11 +0200

    nivm: add max lines for context

--- nvim/lua/user/plugins/treesitter-context.lua
+++ nvim/lua/user/plugins/treesitter-context.lua
@@ -1,4 +1,6 @@
 local utils = require("user.utils")
 local ts_context = utils.call_plugin("treesitter-context")
 
-ts_context.setup({})
+ts_context.setup({
+  max_lines = 10, -- How many lines the window should span. Values <= 0 mean no limit.
+})
