c5c21a23cda122cc7ee3396d88c81a409b6e69af
GustavEkberg <krypteratkadaver@gmail.com>
2026-01-09 18:20:06 +0100

    enable diffview

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,20 +2,81 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true })
+
+          -- Text object
+          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      })
+
+      -- Set up which-key integration for git signs
+      local ok, wk = pcall(require, "which-key")
+      if ok then
+        wk.add({
+          { "<leader>gg",   group = "Gitsigns" },
+          { "<leader>ggf",  ":Gitsigns blame<CR>",                     desc = "Blame file" },
+          { "<leader>ggb",  ":Gitsigns blame_line<CR>",                desc = "Blame line" },
+          { "<leader>ggd",  ":Gitsigns diffthis<CR>",                  desc = "Diff current file" },
+          { "<leader>ggs",  ":Gitsigns stage_hunk<CR>",                desc = "Stage hunk" },
+          { "<leader>ggS",  ":Gitsigns stage_buffer<CR>",              desc = "Stage buffer" },
+          { "<leader>ggr",  ":Gitsigns reset_hunk<CR>",                desc = "Reset hunk" },
+          { "<leader>ggu",  ":Gitsigns undo_stage_hunk<CR>",           desc = "Undo stage hunk" },
+          { "<leader>ggR",  ":Gitsigns reset_buffer<CR>",              desc = "Reset buffer" },
+          { "<leader>ggp",  ":Gitsigns preview_hunk<CR>",              desc = "Preview hunk" },
+          { "<leader>ggtb", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle blame" },
+          { "<leader>ggD",  ":Gitsigns diffthis ~<CR>",                desc = "Diff this ~" },
+          { "<leader>ggtd", ":Gitsigns toggle_deleted<CR>",            desc = "Toggle deleted" },
+        })
+      end
     end,
+  },
+  {
+    "sindrets/diffview.nvim",
   },
   {
     "NeogitOrg/neogit",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require('neogit').setup()
+      require('neogit').setup({
+        kind = "floating",
+        integrations = {
+          diffview = true,
+        },
+      })
 
-      -- Set up which-key integration
+      -- Set up which-key integration for Neogit and diffview
       local ok, wk = pcall(require, "which-key")
       if ok then
         wk.add({
-          { "<leader>go", ":Neogit<CR>", desc = "Open Neogit" },
+          { "<leader>g",  group = "Git" },
+          { "<leader>go", ":Neogit<CR>",                desc = "Open Neogit" },
+          { "<leader>gl", ":Neogit kind=log<CR>",       desc = "Git log" },
+          { "<leader>gb", ":Neogit kind=branching<CR>", desc = "Branches" },
+          { "<leader>gc", ":Neogit kind=commit<CR>",    desc = "Commit" },
+          { "<leader>gs", ":Neogit kind=stash<CR>",     desc = "Stash" },
+          { "<leader>gv", ":DiffviewOpen<CR>",          desc = "Toggle diffview" },
+          { "<leader>gh", ":DiffviewFileHistory<CR>",   desc = "File history" },
         })
       end
     end,
--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -289,65 +289,7 @@ return {
     end,
   },
 
-  -- Git integration
-  {
-    "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local utils = require("user.utils")
-      local gitsigns = utils.call_plugin("gitsigns")
 
-      gitsigns.setup({
-        signs = {
-          add          = { text = '│' },
-          change       = { text = '│' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked    = { text = '┆' },
-        },
-        signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-        linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        watch_gitdir = {
-          interval = 1000,
-          follow_files = true,
-        },
-        attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-          ignore_whitespace = false,
-        },
-        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-        sign_priority = 6,
-        update_debounce = 100,
-        status_formatter = nil,  -- Use default
-        max_file_length = 40000, -- Disable if file is longer than this (in lines)
-        preview_config = {
-          -- Options passed to nvim_open_win
-          border = "single",
-          style = "minimal",
-          relative = "cursor",
-          row = 0,
-          col = 1,
-        }
-      })
-
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>g",  group = "Git" },
-        { "<leader>gf", "<cmd>Gitsigns blame<CR>",      desc = "Blame file" },
-        { "<leader>gb", "<cmd>Gitsigns blame_line<CR>", desc = "Blame line" },
-        { "<leader>gd", "<cmd>Gitsigns diffthis<CR>",   desc = "Diff current file" },
-        { "<leader>gs", "<cmd>Gitsigns stage_buffer<CR>", desc = "Stage current file" },
-        { "<leader>gh", "<cmd>Gitsigns stage_hunk<CR>", desc = "Stage current hunk" },
-      })
-    end,
-  },
 
   -- LazyGit integration
   {
